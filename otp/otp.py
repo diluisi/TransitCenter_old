9d0d602e60b6ec1047b03f3882086549972b840f
rickl4 <38917106+rickl4@users.noreply.github.com>
2020-05-30 21:31:03 -0400

    added some capability to write files

--- otp/otp.py
+++ otp/otp.py
@@ -25,3 +25,102 @@ class study_area:
         county_ids = ["25017", "25025", "25009", "25021", "25023", "25005", "25027", "33015", "33017"]
         region = 'Boston'
         
+class geometry:
+    def boundaries(region, county_ids, **kwargs):
+        
+        # flag to indicate whether the data should be returned in memory
+        in_memory = kwargs.get('in_memory', False)
+        
+        # get the geometry for counties
+        gurl = " https://gist.githubusercontent.com/jamaps/6d94faec77dde5df0f4cdf8d5018ca9d/raw/7d65b613cee762f7224baf60db33b00e903aee1c/US_counties_wgs84.geojson"
+        gdf_counties = gpd.read_file(urlopen(gurl))
+        gdf_counties = gdf_counties[gdf_counties.GEOID.isin(county_ids)]
+        gdf_counties.to_file("../spatial/" + region + "_counties.geojson", driver='GeoJSON')
+    
+        # define the region boundaries
+        gdf_region = gdf_counties.dissolve(by="LSAD")
+        gdf_region["name"] = region 
+        gdf_region = gdf_region[["geometry","name"]]
+        gdf_region.to_file("../spatial/" + region + "_boundary.geojson", driver='GeoJSON')
+        
+        if in_memory == True:
+            return gdf_region
+        else:
+            gdf_region.to_file("../spatial/" + region + "_boundary.geojson", driver='GeoJSON')
+        
+        
+        
+    def block_groups(region, county_ids):
+        
+  
+        
+        # download and into a pandas dataframe
+        burl = "https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG.txt"
+        pd_block_groups = pd.read_csv(urlopen(burl))
+        
+        # string for the full county GEO ID
+        pd_block_groups["county_geoid_temp"] = 1000 * pd_block_groups["STATEFP"] + pd_block_groups["COUNTYFP"]
+        pd_block_groups["CGEOID"] = pd_block_groups["county_geoid_temp"].astype(str)
+        pd_block_groups["CGEOID"][pd_block_groups["county_geoid_temp"] < 10000] = "0" + pd_block_groups["CGEOID"]
+        
+        # subset to just our counties
+        pd_block_groups = pd_block_groups[pd_block_groups.CGEOID.isin(county_ids)]
+        
+        # string for the full block group GEO ID
+        pd_block_groups["block_geoid_temp"] = 1000 * 10000000 * pd_block_groups["STATEFP"] + 10000000 * pd_block_groups["COUNTYFP"] + 10 * pd_block_groups["TRACTCE"] + pd_block_groups["BLKGRPCE"] 
+        pd_block_groups["GEOID"] = pd_block_groups["block_geoid_temp"].astype(str)
+        pd_block_groups["GEOID"][pd_block_groups["block_geoid_temp"] < 100000000000] = "0" + pd_block_groups["GEOID"]
+        
+        # delete the temp columns
+        del pd_block_groups["block_geoid_temp"]
+        del pd_block_groups["county_geoid_temp"]
+        
+        # remove the block groups with an ID of 0, these pertain to those in non-tracted area (usually a waterbody)
+        pd_block_groups = pd_block_groups[pd_block_groups["BLKGRPCE"] > 0]
+
+        
+        burl = "https://www2.census.gov/geo/tiger/GENZ2019/shp/cb_2019_us_bg_500k.zip"
+        gdf_block_groups_poly = gpd.read_file(burl)
+        
+        # add a field for the county
+        gdf_block_groups_poly["CGEOID"] = gdf_block_groups_poly["STATEFP"] + gdf_block_groups_poly["COUNTYFP"]
+        
+        # subset to just our counties
+        gdf_block_groups_poly = gdf_block_groups_poly[gdf_block_groups_poly.CGEOID.isin(county_ids)]
+        
+        # remove the block groups with an ID of 0, these pertain to those in non-tracted area (usually a waterbody)
+        gdf_block_groups_poly = gdf_block_groups_poly[gdf_block_groups_poly["BLKGRPCE"] != "0"]
+    
+        
+        
+    def osm_bounds(region, county_ids, **kwargs):
+
+        file = kwargs.get('file', True)
+        
+        if file == True:
+            gdf_boundary = gpd.read_file("../spatial/" + region + "_boundary.geojson")
+        else:
+            gdf_boundary = geometry.boundaries(region, county_ids, in_memory = True)
+        
+        xmin = float(gdf_boundary.bounds.minx) - 0.05
+        xmax = float(gdf_boundary.bounds.maxx) + 0.05
+        ymin = float(gdf_boundary.bounds.miny) - 0.05
+        ymax = float(gdf_boundary.bounds.maxy) + 0.05
+
+        command_for_clip = "osmconvert us-latest.osm.pbf -b=" + str(xmin) + "," + str(ymin) + "," + str(xmax) + "," + str(ymax) + " -o=" + region + ".osm.pbf"
+
+        return (command_for_clip)
+
+class build:
+    
+    def build_otp():
+        
+        result = subprocess.run(['java', '-Xmx10G', '-jar', 'otp/otp-1.4.0-shaded.jar', '--build', os.getcwd()+'/otp',
+                        '--analyst'], stdout=subprocess.PIPE)
+
+        with open('build_otp_log.txt', 'w') as f:
+            f.truncate()
+            f.write(result.stdout.decode())
+            f.close()
+
+
