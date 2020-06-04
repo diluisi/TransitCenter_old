f0f396f6f0c3259569d5c73be0bf1545a416b3b8
jamaps <jeffallen89@gmail.com>
2020-06-04 10:53:19 -0400

    add some To Do notes

--- accessibility/acc.py
+++ accessibility/acc.py
@@ -1,1 +1,9 @@
+# measuring accessibility
 
+# To Do:
+
+# utils functions for downloading data
+
+# gen functions for measuring accessibility
+
+# have this script to read in data by region and time period, then use the functions, then save the output
