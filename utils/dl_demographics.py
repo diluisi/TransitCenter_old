bbfae5cefc4c7636267def25e540812d59307ee6
jamaps <jeffallen89@gmail.com>
2020-07-09 09:21:41 -0400

    include downloading lehd labour force data

--- utils/dl_demographics.py
+++ utils/dl_demographics.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import cenpy
+import lehd
 
 def dl(region, variables):
+    """
+    Downloads data from the ACS and LEHD
+    """
 
     county_id_path = "data/nationwide_data/county_ids.csv"
 
@@ -14,24 +18,32 @@ def dl(region, variables):
     # setting up the connection to the census API
     conn = cenpy.products.APIConnection("ACSDT5Y2018")
 
-    # loop over each county, downloading data
+
+    # create a unique list of states
+    counties = []
+
+    # loop over each county, downloading data from census
     dfs = []
     for county in county_ids['county_id'].to_list():
         data = conn.query(variables, geo_unit = 'block group', geo_filter = {"state": county[:2],"county": county[2:]})
         dfs.append(data)
+
     df = pd.concat(dfs)
 
     # creating a combined block group csv
     df["geoid"] = df["state"] + df["county"] + df["tract"] + df["block group"]
     del df["state"], df["county"], df["tract"], df["block group"]
 
+
+    # downloading LEHD data
+    dfl = lehd.dl_lodes.rac(
+        locations = county_ids['county_id'].to_list(),
+        year = 2017,
+        geography = "BG",
+        )
+
+    df = pd.merge(df,dfl,left_on ="geoid", right_on ="h_geoid_BG", how = "outer")
+
+    del df["h_geoid_BG"]
+
     df.to_csv(out_data_path, index = False)
-
-    
-
-
-    #
-    #
-    # data = conn.query(["B01001_001E"], geo_unit = 'block group', geo_filter = {"state": "04","county": "019"})
-    #
-    # print(data)
