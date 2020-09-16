fb87a7e2d103492c9352479d2ca5d0d810edb311
Diego <luiz.diego5@gmail.com>
2020-09-16 11:32:49 -0400

    Update fare.py
    Bug description: In some cases, OTP generates trips with two consecutive legs with the same route.
    Problem: The fare calculator was changing the flag and return an error
    Solution: Fare will not correct the trip generated by OTP, in this case, each leg will be charged.

--- fare/fare.py
+++ fare/fare.py
@@ -7,16 +7,8 @@ Created on Thu Jul 30 22:53:37 2020
 """
 import sqlite3
 import json
-import os
-import sys
 
-from pathlib import Path
-fare_path = str(Path(os.getcwd()).parent) + '/fare'
-
-
-
-DB_NAME = fare_path + '/FareDB.db'
-print(DB_NAME)
+DB_NAME = 'FareDB.db'
 
 # recebe somente trips que possuem um caminho alcançável
 def list_mode(json_trip):
@@ -173,6 +165,7 @@ def rule_beautifier(leg_item,transfer_rules, transfer_list, rule_id_true):
                     rule_id_true.append(j[0])
                 # regras que serão utilizadas para atualização
                 rules_choosed.append(transfer_list.index([j[0], j[9], j[10], j[11],leg_item,j[12],status]))
+                print(rules_choosed)
 
     return transfer_list, rule_id_true, rules_choosed 
 
@@ -183,6 +176,7 @@ def transfer_update(rules_id_true,leg_item,leg_duration,flag,transfer_list,curre
     # ajusta a lista de regras
     tnsfr_lst, rl_id_true, rls_choosed = rule_beautifier(leg_item,transfer_rules, transfer_list,rules_id_true)
     # verifica se a lista de regras escolhidas de transferência está vazia
+    #print('entrei aqui')
     if rls_choosed:
         cost = tnsfr_lst[rls_choosed[0]][3]
         flag = 1
@@ -249,13 +243,13 @@ def fare(jsn, region):
             else:
                 return exc_fare/100
         else:
-            
             partial_cost=[]
             flag=0
-            
+            print(flag)
             for leg_index, leg_item in enumerate(control_lst):
                 # setup variables for the first leg, for next looping variables will be update by swapping values
                 if leg_index < (len(control_lst) - 1):
+                    print(leg_item)
                     # Variables setup 
                     previous_agency_id      = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['agencyId'] #ok
                     previous_agency_name    = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['agencyName'] #ok
@@ -270,7 +264,9 @@ def fare(jsn, region):
                     leg_duration            = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['duration']
                     #start_time              =
                     #end_time                =
+                    print(previous_agency_id,previous_agency_name,previous_route_type,previous_route_id,previous_stop_id,next_agency_id,next_route_type,next_route_id,next_stop_id)
                 else:
+                    print(leg_item)
                     previous_agency_id      = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['agencyId'] #ok
                     previous_agency_name    = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['agencyName'] #ok
                     previous_route_type     = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['routeType'] #ok
@@ -281,36 +277,42 @@ def fare(jsn, region):
                         next_route_type         = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['routeType'] #ok
                         next_route_id           = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['routeId'].split(':')[1] #ok
                         next_stop_id            = jsn['OTP_itinerary_all']['plan']['itineraries'][0]['legs'][control_lst[leg_index]]['to']['stopId'].split(':')[1] #ok
+                        
 
                 if flag == 0:
                     # se não for o último elemento e a flag for zero
                     if leg_index < (len(control_lst) - 1):
                         
                         exc_fare = query_exceptions(previous_agency_id,region_id,previous_route_type,previous_route_id)
-                        
-                        if exc_fare == None: 
+                                           
+                        if exc_fare == None:
                             # Query Agency and verify fare_type_id
+                            
                             fare_type_id = query_agency(previous_agency_id,previous_agency_name,region_id,previous_route_type)
-                            print(fare_type_id)
+                            #print(fare_type_id)
                             # Query for each type of fare
                             if fare_type_id == 1:
                                 fare = query_static_fare(previous_agency_id,previous_route_type,region_id)
                                 partial_cost.append(fare)
-                                
+                                transfer_list, flag, cost,rule_true = transfer_update(rule_true,leg_item,leg_duration,flag,transfer_list,next_agency_id,next_route_type,next_route_id,next_stop_id,previous_agency_id,previous_route_type,previous_route_id,previous_stop_id,region_id)
+                                partial_cost.append(cost)
                             elif fare_type_id == 2:
                                 fare = query_zone_fare(previous_agency_id,previous_agency_name,region_id,previous_route_type,
                                                 query_zone(previous_agency_id,previous_agency_name,region_id,previous_route_type,previous_stop_id),
                                                 query_zone(previous_agency_id,previous_agency_name,region_id,previous_route_type,next_stop_id))
                                 partial_cost.append(fare)
+                                transfer_list, flag, cost,rule_true = transfer_update(rule_true,leg_item,leg_duration,flag,transfer_list,next_agency_id,next_route_type,next_route_id,next_stop_id,previous_agency_id,previous_route_type,previous_route_id,previous_stop_id,region_id)
+                                partial_cost.append(cost)
                             else:
                                 fare = 0
                                 partial_cost.append(fare)
                         else:
-                            
                             partial_cost.append(exc_fare)
-                        
-                        transfer_list, flag, cost,rule_true = transfer_update(rule_true,leg_item,leg_duration,flag,transfer_list,next_agency_id,next_route_type,next_route_id,next_stop_id,previous_agency_id,previous_route_type,previous_route_id,previous_stop_id,region_id)
-                        partial_cost.append(cost) # append valor que retorna do transfer dict
+                        #transfer_list, flag, cost,rule_true = transfer_update(rule_true,leg_item,leg_duration,flag,transfer_list,next_agency_id,next_route_type,next_route_id,next_stop_id,previous_agency_id,previous_route_type,previous_route_id,previous_stop_id,region_id)
+                        print(rule_true)
+                        #partial_cost.append(cost) # append valor que retorna do transfer dict
+                        print("partial",partial_cost)
+                        print("flag ", flag)
                    # se for o último elemento da lista e a flag for zero
                     else:
                         exc_fare = query_exceptions(previous_agency_id,region_id,previous_route_type,previous_route_id) 
@@ -345,62 +347,3 @@ def fare(jsn, region):
             print(partial_cost)
             fare =  sum(partial_cost)    
     return fare/100
-# =============================================================================
-# 
-#     
-# if __name__ == '__main__':
-#     
-#     # walk trip
-#     test_case_1 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/test_case_trip_walk.json' #0
-#     # missing trip
-#     test_case_2 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/test_case_trip_null.json' #0
-#     # two legs: WALK + BUS
-#     test_case_3 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/test_case_trip_walk_bus.json' #2.00
-#     # two legs: WALK + BUS EXCEPTIONS
-#     test_case_4 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/test_case_trip_walk_bus_exception.json'#5.25
-#     # two legs: WALK + BUS - TRANSFER==1
-#     test_case_5 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/test_case_trip_walk_bus_t1.json' #2.00
-#     # Testes Rick
-#     test_case_6 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK.json' #0
-#     test_case_7 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_BUS_WALK.json' #2.00
-#     test_case_8 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_EXPRESS_WALK.json' #5.25
-#     test_case_9 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_RAIL_WALK_1.json' #2.75
-#     test_case_10 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_RAIL_WALK_2.json' #4.75
-#     test_case_11 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_RAIL_WALK_3.json' #2.75
-#     test_case_12 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_RAIL_WALK_4.json' #2.75
-#     test_case_13 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/0_WALK_SUBWAY_WALK.json' #2.90
-#     test_case_14 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_BUS_BUS_WALK_Boston.json' #2.00
-#     test_case_15 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_BUS_WALK_BUS_WALK_OutsideBoston.json'#2.00
-#     test_case_16 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_BUS_WALK_RAIL_WALK.json' #8.50
-#     test_case_17 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_BUS_WALK_SUBWAY_WALK.json' #4.90
-#     test_case_18 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_EXPRESS_WALK_BUS_WALK.json' #5.25
-#     test_case_19 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_EXPRESS_WALK_SUBWAY_WALK.json' #8.15
-#     test_case_20 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_RAIL_WALK_BUS_WALK.json' #2.40
-#     test_case_21 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_RAIL_WALK_SUBWAY_WALK.json' #9.90
-#     test_case_22 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_SUBWAY_WALK_BUS_WALK.json' #4.90
-#     test_case_23 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/1_WALK_SUBWAY_WALK_RAIL_WALK.json' #10.90
-#     test_case_24 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/2_WALK_BUS_WALK_RAIL_WALK_BUS_WALK.json' #11.00
-#     test_case_25 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/2_WALK_BUS_WALK_RAIL_WALK_SUBWAY_WALK.json' #14.90
-#     test_case_26 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/2_WALK_TRAM_WALK_SUBWAY_WALK_BUS_WALK.json' #7.80
-#     test_case_27 = '/home/diluisi/Documentos/Doutorado/Projeto US/Fare_DB/Test_Set/3_WALK_BUS_BUS_WALK_SUBWAY_WALK_BUS_WALK.json'
-# 
-# 
-# 
-#     with open(test_case_27) as t:
-#         data = json.load(t)
-#     print(list_mode(data))
-#     #print(exc_fare)
-#     fare = fare(data,'Boston')
-#     print(fare)
-#     #transfers = data['OTP_itinerary_all']['plan']['itineraries'][0]['transfers']
-#     #if transfers == 0:
-#     #    print(list_mode(data))
-#         #a = query_exceptions(843,1,3,10739)
-#         #query_zone(1,'MBTA','Boston',2,'Readville')[0][5]
-#     #    a = query_zone_fare(1,'MBTA',2,query_zone(1,'MBTA','Boston',2,'Readville')[0][5],query_zone(1,'MBTA','Boston',2,'Lincoln')[0][5])[0][5]/100
-#     #    print(a)
-# 
-# 
-#  
-# 
-# =============================================================================
