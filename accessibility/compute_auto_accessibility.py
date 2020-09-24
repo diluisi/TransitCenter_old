0d295602d4bc9ef88c02da852462f3da7964b0bc
jamaps <jeffallen89@gmail.com>
2020-09-24 12:08:58 -0400

    config code for accessibility, finishing up auto accessibility functions

---
+++ accessibility/compute_auto_accessibility.py
@@ -0,0 +1,16 @@
+import acc
+
+region_in = "New York"
+
+#acc.get_nexp_beta("median_time_groceries", region_in)
+#acc.get_nexp_beta("median_time_work", region_in)
+
+
+acc.auto_accessibility(region_in, "WE")
+# acc.auto_accessibility_join(region_in, "PM")
+
+# acc.auto_accessibility(region_in, "wkd")
+# acc.auto_accessibility_join(region_in, "wkd")
+
+# acc.auto_accessibility(region_in, "8am")
+# acc.auto_accessibility_join(region_in, "8am")
