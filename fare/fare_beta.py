4a5dc44efdbaa0f7b8e07e5e121d5f3f150d49bd
Diego <luiz.diego5@gmail.com>
2021-03-30 11:09:44 -0400

    Update
    Inclusion:
    - WAMATA within EXCP list
    - SEPTA within MISSING list

--- fare/fare_beta.py
+++ fare/fare_beta.py
@@ -33,10 +33,9 @@ import sqlite3
 DB_NAME = 'FareDB.db'
 # List of agencies using NTD database
 EXCP = ["nj transit bus","nj transit rail", "long island rail road", "metro-north railroad", "shore line east",
-        "mnr hudson rail link","ny waterway"]
+        "mnr hudson rail link","ny waterway","wmata"]
 # Some agencies have no agency id, we are replicating agency name as agency id for those cases
-MISSING_AGENCY_ID = ["santa cruz metro","county connection","foothill transit","chicago transit authority","cecil transit","prince george’s county thebus"]
-
+MISSING_AGENCY_ID = ["septa","santa cruz metro","county connection","foothill transit","chicago transit authority","cecil transit","prince george’s county thebus"]
 
 # Exclude WALK mode and keep only legs with a valid mode
 def list_mode(json_trip):
